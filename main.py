666b7c184a70eef8a4140813136390856084ada9
OneLimeStudio <onelimestudio@gmail.com>
2023-12-27 21:46:13 +0530

    Updates main.py
    -Now Added Abity of ant to come back to base

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 import math
 import time
 
-screenWidth = 1280
-screenHeight = 720
+screenWidth = 1920
+screenHeight = 1080
 pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
@@ -18,55 +18,68 @@ antSize = 20 #pixels
 
 def DrawPath(path: list):
     
-    for i in range(len(path)):
+    # for i in range(len(path)):
        
-        pygame.draw.rect(screen,(255,255,255), pygame.Rect(path[i][0],path[i][1],antSize,antSize))
-        pygame.draw.rect(screen,(255,255,255), pygame.Rect(path[i][0]+20,path[i][1],antSize,antSize))
-        pygame.draw.rect(screen,(255,255,255), pygame.Rect(path[i][0]+30,path[i][1],antSize,antSize))
-        print(path[i][0],path[i][1])
-
+    #     pygame.draw.rect(screen,(255,255,255), pygame.Rect(path[i][0],path[i][1],antSize,antSize))
+    #     time.sleep(0.1)
+    #     print(path[i][0],path[i][1])
+    pass
 class Ant():
 
     def __init__(self,position:list) -> None:
         self.position = position
         self.path = []
         self.pathFound = False
+        self.count = -1
+        
     def lookFood(self):
         
     
-       # for i in range(self.position[0] - antSize,self.position[0]+antSize):
-        #    for j in range(self.position[1] - antSize,self.position[1]+antSize):
+     
        
         self.position[0] = random.choice([self.position[0] - antSize,self.position[0],self.position[0] + antSize])
         self.position[1] = random.choice([self.position[1] - antSize,self.position[1],self.position[1] + antSize])
         #print(self.position)
         
+    def goBack(self):
+        if(self.pathFound == True):
+            pygame.draw.rect(screen,(255,255,255), pygame.Rect(self.path[self.count][0],self.path[self.count][1],antSize,antSize))
+            print(self.path[self.count][0])
+            if self.count == -1*len(self.path):
+                pass
+            else:
+                self.count -=1
+            
+            
+                
 
         
     def Draw(self):
-        pygame.draw.rect(screen,(255,0,255), pygame.Rect(self.position[0],self.position[1],antSize,antSize))
+        if self.pathFound == False:
+            pygame.draw.rect(screen,(255,0,255), pygame.Rect(self.position[0],self.position[1],antSize,antSize))
 
     def pheromone(self):
         if self.pathFound == False:
-            self.path.append(self.position)
-    def checkFood(self,food : list):
-        for obj in food:
-            x = self.position[0]
-            y = self.position[1]
-            # pygame.draw.rect(screen, (0,255,0), (x,y,antSize+50,antSize+50), 0)
-            # for i in range(4):
-            #     pygame.draw.rect(screen, (0,0,0), (x-i,y-i,antSize+50,antSize+50), 1)
+            #print(self.path)
+            self.path += [[self.position[0],self.position[1]]]
+
             
-            distance = math.sqrt((x - obj.position[0])**2 +(y - obj.position[1])**2 )
-            #print(math.sqrt((2*antSize)**2),distance)
-            if(distance <= math.sqrt((2*antSize)**2)+ 4):
-                #print("Truee")
-                pygame.draw.rect(screen,(255,255,255), pygame.Rect(self.position[0],self.position[1],antSize,antSize))    
-                self.pathFound = True
-                #print(self.path)
-                DrawPath(self.path)
-
-
+    def checkFood(self,food : list):
+    
+        if(self.pathFound == False):
+            for obj in food:
+                x = self.position[0]
+                y = self.position[1]
+      
+                distance = math.sqrt((x - obj.position[0])**2 +(y - obj.position[1])**2 )
+     
+                if(distance <= math.sqrt((2*antSize)**2)+ 4 and self.pathFound == False):
+    
+                    pygame.draw.rect(screen,(255,255,255), pygame.Rect(self.position[0],self.position[1],antSize,antSize))    
+                    self.pathFound = True
+     
+                
+       
 
 class Food():
     def __init__(self,position : list) -> None:
@@ -90,7 +103,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     if hasRun == True:
-        for i in range(4):
+        for i in range(5):
             radius = (screenHeight/2) * math.sqrt(random.random())
             theta = random.random() * 2 * 3.14159 
             x = (screenWidth/2) + radius * math.cos(theta)
@@ -105,21 +118,24 @@ while running:
     else:
         for obj in food:
             obj.Draw()
-    # for obj in ants:
-    #     obj.lookFood()
-    #     obj.Draw()
-    #     obj.pheromone()
-    #     obj.checkFood(food)
-    #     if obj.pathFound == True:
-    #         DrawPath(obj.path)
-    obj =ants[0] 
-    obj.lookFood()
-    obj.Draw()
-    obj.pheromone()
-    obj.checkFood(food)   
-    if obj.pathFound == True:
-        print("OKKK")
-        DrawPath(obj.path)
+    for obj in ants:
+        obj.lookFood()
+        obj.Draw()
+        obj.pheromone()
+        obj.checkFood(food)
+        obj.goBack()
+        if obj.pathFound == True:
+            DrawPath(obj.path)
+    # obj =ants[0] 
+    # obj.lookFood()
+    # obj.Draw()
+    # obj.pheromone()
+    
+    # obj.checkFood(food)   
+    # obj.goBack()
+    # if obj.pathFound == True:
+        
+    #     DrawPath(obj.path)
     
     pygame.draw.rect(screen,(0,255,255), pygame.Rect(screenWidth/2,screenHeight/2,20,20))
   
